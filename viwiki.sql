686db795c0f9354877c1159a48e9e40bcc8dedb7
duongkhanhnghia-hh <nghia9dth@gmail.com>
2017-11-27 12:13:38 +0700

    update database

--- viwiki.sql
+++ viwiki.sql
@@ -90,7 +90,7 @@ CREATE TABLE `categories` (
   KEY `index_categories_on_parent_id` (`parent_id`),
   KEY `index_categories_on_lft` (`lft`),
   KEY `index_categories_on_rgt` (`rgt`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,7 +99,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'Công nghệ thông tin',NULL,1,14,0,0,'2017-06-26 17:09:44','2017-11-12 11:30:54'),(2,'Toán học',NULL,15,16,0,0,'2017-06-26 17:09:44','2017-11-12 11:30:54'),(3,'Vật lý',NULL,17,18,0,0,'2017-06-26 17:09:44','2017-11-12 11:30:54'),(4,'Công nghệ phần mềm',1,2,9,1,0,'2017-06-26 17:09:44','2017-06-29 10:09:48'),(5,'Khoa học máy tính',1,10,11,1,0,'2017-06-26 17:09:44','2017-06-29 10:09:48'),(6,'Mạng máy tính',1,12,13,1,0,'2017-06-26 17:09:44','2017-06-29 10:09:48'),(7,'Kiểm thử',4,3,6,2,0,'2017-06-26 17:09:44','2017-06-29 10:09:48'),(8,'Phương pháp hình thức',4,7,8,2,0,'2017-06-26 17:09:44','2017-06-29 10:09:48'),(9,'Tên chuyên mục',7,4,5,3,0,'2017-06-29 10:09:48','2017-06-29 10:09:48');
+INSERT INTO `categories` VALUES (1,'Công nghệ thông tin',NULL,1,12,0,3,'2017-06-26 17:09:44','2017-11-25 03:52:59'),(2,'Toán học',NULL,13,16,0,1,'2017-06-26 17:09:44','2017-11-25 18:57:13'),(3,'Vật lý',NULL,17,18,0,0,'2017-06-26 17:09:44','2017-11-25 18:57:13'),(4,'Công nghệ phần mềm',1,2,7,1,2,'2017-06-26 17:09:44','2017-11-25 03:52:59'),(5,'Khoa học máy tính',1,8,9,1,0,'2017-06-26 17:09:44','2017-11-25 03:52:59'),(6,'Mạng máy tính',1,10,11,1,0,'2017-06-26 17:09:44','2017-11-25 03:52:59'),(7,'Kiểm thử',4,3,4,2,0,'2017-06-26 17:09:44','2017-11-25 03:56:18'),(8,'Phương pháp hình thức',4,5,6,2,0,'2017-06-26 17:09:44','2017-11-25 03:52:59'),(9,'Lịch sử',NULL,19,20,0,0,'2017-06-26 17:09:44','2017-11-12 11:30:54'),(10,'Địa lý',NULL,21,22,0,0,'2017-06-26 17:09:44','2017-11-12 11:30:54'),(11,'Văn học',NULL,23,24,0,0,'2017-06-26 17:09:44','2017-11-12 11:30:54'),(12,'Hóa học',NULL,25,26,0,0,'2017-06-26 17:09:44','2017-11-12 11:30:54'),(21,'Hình học',2,14,15,1,0,'2017-11-25 18:57:12','2017-11-25 18:57:13');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -182,7 +182,7 @@ CREATE TABLE `roles` (
   PRIMARY KEY (`id`),
   KEY `index_roles_on_name` (`name`),
   KEY `index_roles_on_name_and_resource_type_and_resource_id` (`name`,`resource_type`,`resource_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -191,7 +191,7 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'admin',NULL,NULL,'2016-11-26 04:10:33','2016-11-26 04:10:33'),(2,'editor','Category',4,'2016-11-28 04:26:32','2016-11-28 04:26:32'),(3,'editor','Category',7,'2016-11-28 07:24:01','2016-11-28 07:24:01'),(4,'editor','Category',1,'2016-11-26 04:10:33','2016-11-26 04:10:33'),(5,'editor','Category',2,'2016-11-26 04:10:33','2016-11-26 04:10:33'),(6,'editor','Category',3,'2016-11-26 04:10:33','2016-11-26 04:10:33');
+INSERT INTO `roles` VALUES (1,'admin',NULL,NULL,'2016-11-26 04:10:33','2016-11-26 04:10:33'),(2,'editor','Category',4,'2016-11-28 04:26:32','2016-11-28 04:26:32'),(3,'editor','Category',7,'2016-11-28 07:24:01','2016-11-28 07:24:01'),(4,'editor','Category',1,'2016-11-26 04:10:33','2016-11-26 04:10:33'),(5,'editor','Category',2,'2016-11-26 04:10:33','2016-11-26 04:10:33'),(6,'editor','Category',3,'2016-11-26 04:10:33','2016-11-26 04:10:33'),(7,'user',NULL,NULL,'2016-11-26 04:10:33','2016-11-26 04:10:33'),(8,'2',NULL,NULL,NULL,'2017-11-27 03:23:53');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -248,7 +248,7 @@ CREATE TABLE `users` (
   UNIQUE KEY `index_users_on_email` (`email`),
   UNIQUE KEY `index_users_on_reset_password_token` (`reset_password_token`),
   UNIQUE KEY `index_users_on_confirmation_token` (`confirmation_token`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -257,7 +257,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (2,'baongoc124@gmail.com','$2a$11$VmsnPhZSsUehLgfEnAoM1eomW.My7p9eZunbF84cb2BfGniwbNy1W',1,NULL,NULL,NULL,24,'2017-11-12 03:31:30','2017-11-12 03:03:53','127.0.0.1','127.0.0.1','WpEbbK968CLHZwDedLTr','2016-11-25 03:29:38','2016-11-25 03:29:30',NULL,'2016-11-25 03:29:30','2017-11-12 03:31:30'),(3,'test1@t.com','$2a$11$jUUUhfxvh0oM3SyCZpZ.w.uOQe6PAAYsvDir4/UUcowJj07GAfJdm',1,NULL,NULL,NULL,1,'2016-11-25 10:41:23','2016-11-25 10:41:23','::1','::1','7t4m-euH7DUt7GWQJVZF','2016-11-25 10:41:01','2016-11-25 10:40:55',NULL,'2016-11-25 10:40:55','2017-11-08 18:55:41'),(4,'jazmyn@muellermcdermott.co','$2a$11$V2wplnoFzCLjruHhLVVjeewiAXMwRXoYePlJ7V0hv6BZ0mTLB8P9a',1,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,'J6HLabsJGnqDiLWs_KBd',NULL,'2016-11-28 04:44:24',NULL,'2016-11-28 04:44:24','2017-11-08 18:51:20'),(5,'hilbert@ziemannsauer.com','$2a$11$VmsnPhZSsUehLgfEnAoM1eomW.My7p9eZunbF84cb2BfGniwbNy1W',1,NULL,NULL,NULL,2,'2016-11-28 08:17:19','2016-11-28 07:25:19','::1','::1','MHcWNZxpzUy37ammDCAu','2016-11-28 07:22:56','2016-11-28 07:22:47',NULL,'2016-11-28 07:22:47','2016-11-28 08:17:19'),(6,'test1@gmail.com','$2a$11$uTnQ03kmvnpF5sA.W2idXuBaYOW1YMuf3qtP1QFVV1DNC5mn8fpYm',1,NULL,NULL,NULL,1,'2016-11-28 10:51:56','2016-11-28 10:51:56','::1','::1','51LDpzXsAZuCBNyjvUvZ','2016-11-28 10:51:44','2016-11-28 10:51:41',NULL,'2016-11-28 10:51:41','2017-11-08 18:55:29'),(7,'baongoc123@gmail.com','$2a$11$2CSmSknmCM8K51g4b4pvg.ZyXnqj4EZXVkYim0.kZ7n7B3hQVmoY.',0,NULL,NULL,NULL,4,'2017-11-12 03:32:21','2017-11-11 10:58:38','127.0.0.1','127.0.0.1','iqB57CfXrZgzK76_erqm','2017-11-08 10:23:20','2017-11-08 10:23:16',NULL,'2017-11-08 10:23:16','2017-11-12 10:53:33'),(8,'baongoc1241@gmail.com','$2a$11$VmsnPhZSsUehLgfEnAoM1eomW.My7p9eZunbF84cb2BfGniwbNy1W',1,NULL,NULL,NULL,19,'2017-11-07 09:02:57','2017-11-05 17:59:35','127.0.0.1','127.0.0.1','W1pEbbK968C1LHZwDedLTr','2016-11-25 03:29:38','2016-11-25 03:29:30',NULL,'2016-11-25 03:29:30','2017-11-07 09:02:57'),(9,'test1@t1.com','$2a$11$jUUUhfxvh0oM3SyCZpZ.w.uOQe6PAAYsvDir4/UUcowJj07GAfJdm',0,NULL,NULL,NULL,1,'2016-11-25 10:41:23','2016-11-25 10:41:23','::1','::1','17t4m-eqH7qDUt7GWQJVZF','2016-11-25 10:41:01','2016-11-25 10:40:55',NULL,'2016-11-25 10:40:55','2016-11-25 10:41:51'),(10,'1jazmyn@muellermcdermott.co','$2a$11$V2wplnoFzCLjruHhLVVjeewiAXMwRXoYePlJ7V0hv6BZ0mTLB8P9a',0,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,'1J6HLabsJGnqDiLWs_KBd',NULL,'2016-11-28 04:44:24',NULL,'2016-11-28 04:44:24','2016-11-28 04:44:24'),(11,'1hilbert@ziemannsauer.com','$2a$11$VmsnPhZSsUehLgfEnAoM1eomW.My7p9eZunbF84cb2BfGniwbNy1W',1,NULL,NULL,NULL,2,'2016-11-28 08:17:19','2016-11-28 07:25:19','::1','::1','MHcWNZxp1Uy37ammDCAu','2016-11-28 07:22:56','2016-11-28 07:22:47',NULL,'2016-11-28 07:22:47','2016-11-28 08:17:19'),(12,'tes1t1@gmail.com','$2a$11$uTnQ03kmvnpF5sA.W2idXuBaYOW1YMuf3qtP1QFVV1DNC5mn8fpYm',1,NULL,NULL,NULL,1,'2016-11-28 10:51:56','2016-11-28 10:51:56','::1','::1','51LDp1zXsAZuCBNyjvUvZ','2016-11-28 10:51:44','2016-11-28 10:51:41',NULL,'2016-11-28 10:51:41','2017-11-09 12:02:19');
+INSERT INTO `users` VALUES (2,'baongoc124@gmail.com','$2a$11$VmsnPhZSsUehLgfEnAoM1eomW.My7p9eZunbF84cb2BfGniwbNy1W',1,NULL,NULL,NULL,38,'2017-11-27 05:04:12','2017-11-27 03:10:55','127.0.0.1','127.0.0.1','WpEbbK968CLHZwDedLTr','2016-11-25 03:29:38','2016-11-25 03:29:30',NULL,'2016-11-25 03:29:30','2017-11-27 05:04:12'),(3,'ngoctm@xy.com','$2a$11$jUUUhfxvh0oM3SyCZpZ.w.uOQe6PAAYsvDir4/UUcowJj07GAfJdm',1,NULL,NULL,NULL,1,'2016-11-25 10:41:23','2016-11-25 10:41:23','::1','::1','ygnpwxaW76x_Z-QS5YK5','2017-11-26 15:31:35','2017-11-26 15:31:32',NULL,'2016-11-25 10:40:55','2017-11-27 03:29:41'),(4,'jazmyn@muellermcdermott.co','$2a$11$V2wplnoFzCLjruHhLVVjeewiAXMwRXoYePlJ7V0hv6BZ0mTLB8P9a',1,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,'J6HLabsJGnqDiLWs_KBd',NULL,'2016-11-28 04:44:24',NULL,'2016-11-28 04:44:24','2017-11-27 03:48:17'),(5,'hilbert@ziemannsauer.com','$2a$11$VmsnPhZSsUehLgfEnAoM1eomW.My7p9eZunbF84cb2BfGniwbNy1W',0,NULL,NULL,NULL,2,'2016-11-28 08:17:19','2016-11-28 07:25:19','::1','::1','MHcWNZxpzUy37ammDCAu','2016-11-28 07:22:56','2016-11-28 07:22:47',NULL,'2016-11-28 07:22:47','2017-11-21 09:05:33'),(6,'test1@gmail.com','$2a$11$uTnQ03kmvnpF5sA.W2idXuBaYOW1YMuf3qtP1QFVV1DNC5mn8fpYm',0,NULL,NULL,NULL,1,'2016-11-28 10:51:56','2016-11-28 10:51:56','::1','::1','51LDpzXsAZuCBNyjvUvZ','2016-11-28 10:51:44','2016-11-28 10:51:41',NULL,'2016-11-28 10:51:41','2017-11-27 01:40:36'),(7,'baongoc123@gmail.com','$2a$11$2CSmSknmCM8K51g4b4pvg.ZyXnqj4EZXVkYim0.kZ7n7B3hQVmoY.',1,NULL,NULL,NULL,7,'2017-11-25 18:47:54','2017-11-14 02:39:21','127.0.0.1','127.0.0.1','iqB57CfXrZgzK76_erqm','2017-11-08 10:23:20','2017-11-08 10:23:16',NULL,'2017-11-08 10:23:16','2017-11-27 03:48:23'),(8,'baongoc1241@gmail.com','$2a$11$VmsnPhZSsUehLgfEnAoM1eomW.My7p9eZunbF84cb2BfGniwbNy1W',0,NULL,NULL,NULL,19,'2017-11-07 09:02:57','2017-11-05 17:59:35','127.0.0.1','127.0.0.1','W1pEbbK968C1LHZwDedLTr','2016-11-25 03:29:38','2016-11-25 03:29:30',NULL,'2016-11-25 03:29:30','2017-11-26 09:34:30'),(9,'test1@t1.com','$2a$11$jUUUhfxvh0oM3SyCZpZ.w.uOQe6PAAYsvDir4/UUcowJj07GAfJdm',0,NULL,NULL,NULL,1,'2016-11-25 10:41:23','2016-11-25 10:41:23','::1','::1','17t4m-eqH7qDUt7GWQJVZF','2016-11-25 10:41:01','2016-11-25 10:40:55',NULL,'2016-11-25 10:40:55','2016-11-25 10:41:51'),(10,'1jazmyn@muellermcdermott.co','$2a$11$V2wplnoFzCLjruHhLVVjeewiAXMwRXoYePlJ7V0hv6BZ0mTLB8P9a',0,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,'1J6HLabsJGnqDiLWs_KBd',NULL,'2016-11-28 04:44:24',NULL,'2016-11-28 04:44:24','2016-11-28 04:44:24'),(11,'1hilbert@ziemannsauer.com','$2a$11$VmsnPhZSsUehLgfEnAoM1eomW.My7p9eZunbF84cb2BfGniwbNy1W',0,NULL,NULL,NULL,2,'2016-11-28 08:17:19','2016-11-28 07:25:19','::1','::1','MHcWNZxp1Uy37ammDCAu','2016-11-28 07:22:56','2016-11-28 07:22:47',NULL,'2016-11-28 07:22:47','2017-11-21 09:05:55'),(12,'tes1t1@gmail.com','$2a$11$uTnQ03kmvnpF5sA.W2idXuBaYOW1YMuf3qtP1QFVV1DNC5mn8fpYm',0,NULL,NULL,NULL,1,'2016-11-28 10:51:56','2016-11-28 10:51:56','::1','::1','51LDp1zXsAZuCBNyjvUvZ','2016-11-28 10:51:44','2016-11-28 10:51:41',NULL,'2016-11-28 10:51:41','2017-11-21 09:05:49'),(13,'mobefothotxacemyeu@yahoo.com','$2a$11$FHeYjNNyFfSH5Zf2F6nHPe/6qKwfmyKzFXG50T9A8ji.uCBMPlSQi',0,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,'HUM8nMA2kFSvZGse4_Rr','2017-11-26 09:43:00','2017-11-26 09:42:54',NULL,'2017-11-26 09:42:54','2017-11-26 09:43:00');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -281,7 +281,7 @@ CREATE TABLE `users_roles` (
 
 LOCK TABLES `users_roles` WRITE;
 /*!40000 ALTER TABLE `users_roles` DISABLE KEYS */;
-INSERT INTO `users_roles` VALUES (2,1),(3,4),(4,1),(4,1),(4,6),(5,1),(5,3),(6,3),(7,2),(7,2),(7,5),(12,1);
+INSERT INTO `users_roles` VALUES (2,1),(2,2),(3,7),(4,7),(7,1);
 /*!40000 ALTER TABLE `users_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -294,4 +294,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-12 21:23:26
+-- Dump completed on 2017-11-27 12:12:38
